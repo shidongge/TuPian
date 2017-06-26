94765749145366122f2577f4691553aac1c72b38
shido <https://github.com/shidongge/APPdajian>
2017-06-26 19:45:58 +0800

    沉浸式一体篮

---
+++ YiTiLan/src/main/java/cn/bgs/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package cn.bgs.myapplication;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //透明状态栏
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+            Window window = getWindow();
+            window.setFlags( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    }
+}
